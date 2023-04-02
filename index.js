37112169cfbaab3de1ddc9430ccca144a5ed175f
kamalkumar7 <202051098@iiitvadodara.ac.in>
2023-04-03 01:45:55 +0530

    first commit

--- index.js
+++ index.js
@@ -28,16 +28,16 @@ app.get('/hi',(req,res)=>{
 })
 dotenv.config();
 
-const connect = () => {
-  mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-      console.log("Connected to DB");
-    })
-    .catch((err) => {
-      throw err;
-    });
-};
+// const connect = () => {
+//   mongoose
+//     .connect(process.env.MONGO)
+//     .then(() => {
+//       console.log("Connected to DB");
+//     })
+//     .catch((err) => {
+//       throw err;
+//     });
+// };
 
 //middlewares
 app.use(cookieParser())
